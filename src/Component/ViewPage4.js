fcd308224266085fe2f56862d15f6bf1bc9ff5c7
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-22 23:58:41 +0900

    Create ViewPageNumber Component

---
+++ src/Component/ViewPage4.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import TopBar from './TopBar';
+import Nav from './Nav';
+
+class ViewPage4 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <TopBar>
+
+                </TopBar>
+                <Nav>
+
+                </Nav>
+                
+            </div>
+         );
+    }
+}
+ 
+export default ViewPage4;

c71a503cbd13ddb53af9330a31ace02331b8c69d
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-23 23:56:36 +0900

    Add DownBtn to AnyEvent

--- src/Component/ViewPage4.js
+++ src/Component/ViewPage4.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
 import TopBar from './TopBar';
 import Nav from './Nav';
+import '../Css/viewpage.css';
+import { onclick } from '../Funtion';
+import BottomBtn from '../Image/downBtn.png';
+
 
 class ViewPage4 extends Component {
     constructor(props) {
@@ -10,14 +13,14 @@ class ViewPage4 extends Component {
     }
     render() { 
         return ( 
-            <div>
+            <div id="page4"  className="View_Main_Page">
                 <TopBar>
 
                 </TopBar>
                 <Nav>
 
                 </Nav>
-                
+                <img onClick={onclick} src={BottomBtn} alt="하단 버튼"/>
             </div>
          );
     }

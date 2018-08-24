d5d46928b33e5872d6c8b39c934f329529caf1fe
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-24 17:24:47 +0900

    Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React,{Component} from 'react';
-// import Main from './container/Main/Main'
-import {SectionsContainer, Section } from 'react-fullpage';
+import {SectionsContainer, Section, Header, Footer } from 'react-fullpage';
 import MainDefaultLayout from './components/Main/MaindefaultLayout'
-import Main from './container/Main/Main'
 
 class App  extends Component {
   constructor(props) {
@@ -10,17 +8,13 @@ class App  extends Component {
     this.state = {
       options : {
         sectionClassName : 'section',
-        anchors : ['page1', 'page2', 'page3', 'page4', 'page5'],
+        anchors : ['page1', 'page2', 'page3', 'page4','page5'],
       }
     }
   }
   render() {
     return ( 
-      <div>
         <SectionsContainer {...this.state.options}>
-          <Section>
-            <MainDefaultLayout PageNum={1}/>
-          </Section>
           <Section>
             <MainDefaultLayout PageNum={2}/>
           </Section>
@@ -30,11 +24,13 @@ class App  extends Component {
           <Section>
             <MainDefaultLayout PageNum={4}/>
           </Section>
-          <Section onChange={this.handleSectionChange}>
+          <Section>
             <MainDefaultLayout PageNum={5}/>
           </Section>
+          <Section>
+            <MainDefaultLayout PageNum={6}/>
+          </Section>
         </SectionsContainer>
-      </div>
      );
   }
 }

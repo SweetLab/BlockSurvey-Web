1eb8ebee6330b893f9395432a0c02365005a0f42
GwangMinBack <bgm8862@gmail.com>
2018-08-24 17:23:54 +0900

    fullpage librery change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 // import Main from './container/Main/Main'
-import {SectionsContainer, Section } from 'react-fullpage';
+import ReactFullpage from '@fullpage/react-fullpage';
 import MainDefaultLayout from './components/Main/MaindefaultLayout'
 import Main from './container/Main/Main'
 
@@ -8,32 +8,34 @@ class App  extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      options : {
-        sectionClassName : 'section',
-        anchors : ['page1', 'page2', 'page3', 'page4', 'page5'],
-      }
+      fullpageOptions : {
+        callbacks: ['onLeave'],
+      },
     }
   }
   render() {
     return ( 
       <div>
-        <SectionsContainer {...this.state.options}>
-          <Section>
+        <ReactFullpage {...this.state.fullpageOptions} 
+        render={({ state, fullpageApi }) => (
+          <div>
+          <div className="section">
             <MainDefaultLayout PageNum={1}/>
-          </Section>
-          <Section>
+          </div>
+          <div className="section">
             <MainDefaultLayout PageNum={2}/>
-          </Section>
-          <Section>
+          </div>
+          <div className="section">
             <MainDefaultLayout PageNum={3}/>
-          </Section>
-          <Section>
+          </div>
+          <div className="section">
             <MainDefaultLayout PageNum={4}/>
-          </Section>
-          <Section onChange={this.handleSectionChange}>
+          </div>
+          <div className="section">
             <MainDefaultLayout PageNum={5}/>
-          </Section>
-        </SectionsContainer>
+          </div>
+        </div>
+        )}/>
       </div>
      );
   }

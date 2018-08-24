007076c6487f8e0bd04b51a8736b3b3c4a61dbb3
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-24 16:42:55 +0900

    Add Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React,{Component} from 'react';
+// import Main from './container/Main/Main'
+import {SectionsContainer, Section } from 'react-fullpage';
+import MainDefaultLayout from './components/Main/MaindefaultLayout'
 import Main from './container/Main/Main'
+
 class App  extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      options : {
+        sectionClassName : 'section',
+        anchors : ['page1', 'page2', 'page3', 'page4', 'page5'],
+      }
+    }
   }
-  render() { 
+  render() {
     return ( 
-      <Main/>
+      <div>
+        <SectionsContainer {...this.state.options}>
+          <Section>
+            <MainDefaultLayout PageNum={1}/>
+          </Section>
+          <Section>
+            <MainDefaultLayout PageNum={2}/>
+          </Section>
+          <Section>
+            <MainDefaultLayout PageNum={3}/>
+          </Section>
+          <Section>
+            <MainDefaultLayout PageNum={4}/>
+          </Section>
+          <Section onChange={this.handleSectionChange}>
+            <MainDefaultLayout PageNum={5}/>
+          </Section>
+        </SectionsContainer>
+      </div>
      );
   }
 }

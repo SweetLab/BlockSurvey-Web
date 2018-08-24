4e91f97d2e3cca2a4db65c6a9c5f1e5568cd4839
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-24 22:19:29 +0900

    Main container

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import {ClickScroll} from '../../core/ScrollEvent'
-import MainDefaultLayout from '../../components/Main/MaindefaultLayout'
+import TopBar from '../../components/Main/TopBar';
+import Navi from '../../components/Main/Navi';
+import CenterBlock from '../../components/Main/CenterBlock';
+import CenterSurvey from '../../components/Main/CenterSurvey';
+import CenterMyPage from '../../components/Main/CenterMyPage';
+import CenterHelpPage from '../../components/Main/CenterHelpPage';
+import CenterContact from '../../components/Main/CenterContact';
+import ReactFullpage from '@fullpage/react-fullpage';
+import '../../components/Main/css/viewpage.css';
 
 class Main extends Component {
   render() { 
     return (
-      <div onScroll={ClickScroll()} className="App_Main">
-        <MainDefaultLayout PageNum={1}/>
-        <MainDefaultLayout PageNum={2}/>
-        <MainDefaultLayout PageNum={3}/>
-        <MainDefaultLayout PageNum={4}/>
-        <MainDefaultLayout PageNum={5}/>
-      </div>
+      
+      <div>
+          <ReactFullpage {...this.props.ReactFullpage} render={() => {
+            return (
+              <div className="View_Main_Page">
+                  <div id="page1" className="section">
+                  <TopBar />
+                  <Navi />
+                  <CenterBlock/>
+                </div>
+                <div id="page2" className="section">
+                  <TopBar />
+                  <Navi />
+                  <CenterSurvey/>
+                </div>
+                <div id="page3" className="section">
+                  <TopBar />
+                  <Navi />
+                  <CenterMyPage/>
+                </div>
+                <div id="page4" className="section">
+                  <TopBar />
+                  <Navi />
+                  <CenterHelpPage/>
+                </div>
+                <div id="page5" className="section">
+                  <TopBar />
+                  <Navi />
+                  <CenterContact/>
+                </div>
+              </div>
+            )
+          }}/> 
+        </div>
     );
   }
 }

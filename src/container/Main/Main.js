9132c15132686e909dcba66bcb1263ce64d7953c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-24 16:43:33 +0900

    Change state

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -6,11 +6,11 @@ class Main extends Component {
   render() { 
     return (
       <div onScroll={ClickScroll()} className="App_Main">
-        <MainDefaultLayout PageNum='1'/>
-        <MainDefaultLayout PageNum='2'/>
-        <MainDefaultLayout PageNum='3'/>
-        <MainDefaultLayout PageNum='4'/>
-        <MainDefaultLayout PageNum='5'/>
+        <MainDefaultLayout PageNum={1}/>
+        <MainDefaultLayout PageNum={2}/>
+        <MainDefaultLayout PageNum={3}/>
+        <MainDefaultLayout PageNum={4}/>
+        <MainDefaultLayout PageNum={5}/>
       </div>
     );
   }

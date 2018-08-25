f8d4fe6e55680de9c762d1f97835175df34e64bd
GwangMinBack <bgm8862@gmail.com>
2018-08-25 20:30:02 +0900

    네비 고정 위치

--- src/components/Main/MaindefaultLayout.js
+++ src/components/Main/MaindefaultLayout.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import Navi from './Navi';
-import TopBar from './TopBar';
 
 class MaindefaultLayout extends Component {
   render() {
     const {pageId} = this.props;
     return (
       <div className="section" id={`page${pageId}`}>
-        <TopBar />
-        <Navi />
         {this.props.children}
       </div>
         

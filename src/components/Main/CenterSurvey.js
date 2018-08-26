1e940cf4cac76bc21745c1da9f7fea4ed8e4558f
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-26 22:18:52 +0900

    CenterSurvey Block

--- src/components/Main/CenterSurvey.js
+++ src/components/Main/CenterSurvey.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import './css/centerSurvey.css';
 
 class CenterSurvey extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
         return ( 
-            <div>
-
+            <div className="BlockSurvey">
+                <div className="Survey_title_1">
+                    Participate in <span className="Survey_Bold_title_1">Survey</span>
+                </div>
+                <div className="Contact_btn_1">
+                    Paricipate
+                </div>
+                <div className="Contact_btn_2">
+                    Make
+                </div>
+                <div className="Survey_title_2">
+                    <span className="Survey_min_title_2">Make</span> Survey
+                </div>
             </div>
          );
     }

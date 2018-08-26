12a7fcf4da73161949a77c9d91efb18227edd123
GwangMinBack <bgm8862@gmail.com>
2018-08-26 21:47:47 +0900

    애니메이션 수정 및 구현

--- src/components/Main/CenterContact.js
+++ src/components/Main/CenterContact.js
@@ -5,13 +5,13 @@ class CenterContact extends Component {
     render() { 
         return ( 
             <div className="BlockContact">
-                <div className="Contact_title">
+                <div className="Block_title">
                     Contact Us
                 </div>
-                <div className="Contact_msg">
+                <div className="Block_msg">
                     Thank you for visiting <span>BlockSurvey</span>
                 </div>
-                <div className="Contact_btn">
+                <div className="Block_btn">
                     Contact <span className="btn_arrow">➔</span>
                 </div>
             </div>

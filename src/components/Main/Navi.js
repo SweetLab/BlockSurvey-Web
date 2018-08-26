5ae1c07a0c646ba0ad8628cf50f5ee9b70f168a7
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-26 10:30:25 +0900

    map file error I need help

--- src/components/Main/Navi.js
+++ src/components/Main/Navi.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import '../../components/Main/css/navigation.css';
 
 class Navi extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
         return ( 
-            <div></div>
+            <div className="navigation">
+                {this.NaviContent()}
+            </div>
          );
+    }
+    NaviContent() {
+        const { content } = this.props
+        return content.map(item => (
+            <div className="content">
+                <div className={`content-nav-${item.contentState}-on`}>
+                    <span className="content-nav-text">{item.ContentTitle}</span>
+                </div>
+                <div className="content-circle"></div>
+            </div>
+        ))
     }
 }
  
--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
-import CenterBlock from '../../components/Main/CenterBlock';
-import CenterSurvey from '../../components/Main/CenterSurvey';
-import CenterMyPage from '../../components/Main/CenterMyPage';
-import CenterHelpPage from '../../components/Main/CenterHelpPage';
-import CenterContact from '../../components/Main/CenterContact';
-import ReactFullpage from '@fullpage/react-fullpage';
-import '../../components/Main/css/viewpage.css';
+import React, { Component } from 'react'
+import CenterBlock from '../../components/Main/CenterBlock'
+import CenterSurvey from '../../components/Main/CenterSurvey'
+import CenterMyPage from '../../components/Main/CenterMyPage'
+import CenterHelpPage from '../../components/Main/CenterHelpPage'
+import CenterContact from '../../components/Main/CenterContact'
+import ReactFullpage from '@fullpage/react-fullpage'
+import '../../components/Main/css/viewpage.css'
 import MaindefaultLayout from '../../components/Main/MaindefaultLayout'
 import Navi from '../../components/Main/Navi'
 import TopBar from '../../components/Main/TopBar'
+// import { onChangeMain } from '../../core/ScrollEvent'
 
 class Main extends Component {
+  state = {
+    content : {
+      contentTitle : [`B`, `S`, `M`, `H`, `C`],
+      contentState : [`1`,`2`,`3`,`4`,`5`]
+    }
+  }
   render() { 
     return (
       <div>
-        <Navi/>
+        <Navi
+          content={this.state.content}
+         /> 
         <TopBar />
           <ReactFullpage {...this.props.ReactFullpage} render={() => {
             return (

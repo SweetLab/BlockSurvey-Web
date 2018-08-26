49dfe72a0c573b69cb726025780617d837ca9988
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-26 22:45:51 +0900

    수정

--- src/components/Main/CenterSurvey.js
+++ src/components/Main/CenterSurvey.js
@@ -8,11 +8,15 @@ class CenterSurvey extends Component {
                 <div className="Survey_title_1">
                     Participate in <span className="Survey_Bold_title_1">Survey</span>
                 </div>
-                <div className="Contact_btn_1">
-                    Paricipate
+                <div className="Survey_btn_1">
+                    <div className="Block_btn">
+                        Paricipate<span className="btn_arrow">➔</span>
+                    </div>
                 </div>
-                <div className="Contact_btn_2">
-                    Make
+                <div className="Survey_btn_2">
+                    <div className="Block_btn">
+                        Make<span className="btn_arrow">➔</span>
+                    </div>
                 </div>
                 <div className="Survey_title_2">
                     <span className="Survey_min_title_2">Make</span> Survey

3247f914c7a8ff1668330a1ba5d647b829114663
GwangMinBack <bgm8862@gmail.com>
2018-08-27 09:14:14 +0900

    Router 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React,{Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Main from './container/Main/Main';
 
 class App  extends Component {
   render() {
-    return ( 
-        <Main />
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Main} exact/>
+        </Switch>
+      </BrowserRouter>
      );
   }
 }

f9b863e5b5689904487cc89eba30e694fc86e2af
seoyunho <yunho101775@gmail.com>
2018-08-28 22:27:25 +0900

    merge error 수정:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React,{ Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Main, Participate, CreateSurvey } from './container/index'
-=======
 import React,{Component} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import {Main, Participate, MyPage} from './container/index'
+import {Main, Participate, MyPage, CreateSurvey} from './container/index'
 import BlockSurvey from './core/BlockSurvey'
->>>>>>> 0059cc9f9cf222378807e1b66d7873aa12b00dd7
 
 class App  extends Component {
   render() {
@@ -16,15 +10,10 @@ class App  extends Component {
         <BlockSurvey/>
         <BrowserRouter>
           <Switch>
-<<<<<<< HEAD
-            <Route path='/' component = { Main } exact/>
-            <Route path='/survey/participate' component = { Participate } exact/>
-            <Route path='/survey/create' component = { CreateSurvey } exact/>
-=======
             <Route path='/' component={Main} exact/>
             <Route path='/survey/participate' component={Participate} exact/>
             <Route path = '/mypage' component = { MyPage } exact/>
->>>>>>> 0059cc9f9cf222378807e1b66d7873aa12b00dd7
+            <Route path='/survey/create' component = { CreateSurvey } exact/>
           </Switch>
         </BrowserRouter>
       </div>
--- src/container/index.js
+++ src/container/index.js
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 import Main from './Main/Main';
 import Participate from './Participate/Participate';
+import MyPage from './MyPage/MyPage';
+import SurveyJoin from './Participate/SurveyJoin';
 import CreateSurvey from './Survey/CreateSurvey';
-
-export { Main, Participate, CreateSurvey };
-=======
-import Main from './Main/Main'
-import Participate from './Participate/Participate'
-import MyPage from './MyPage/MyPage'
-import SurveyJoin from './Participate/SurveyJoin'
 
 
 export {
   Main,
   Participate,
   MyPage,
-  SurveyJoin
+  SurveyJoin,
+  CreateSurvey
 };
->>>>>>> 0059cc9f9cf222378807e1b66d7873aa12b00dd7

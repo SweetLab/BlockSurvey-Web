e294a251d7dbd8ad776478dc1ef9bcbd669919ca
GwangMinBack <bgm8862@gmail.com>
2018-08-28 23:10:13 +0900

    react-cookie, toastr 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React,{Component} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Main, Participate, MyPage} from './container/index'
-import BlockSurvey from './core/BlockSurvey'
 
 class App  extends Component {
   render() {
     return (
       <div id="App">
-        <BlockSurvey/>
         <BrowserRouter>
           <Switch>
             <Route path='/' component={Main} exact/>

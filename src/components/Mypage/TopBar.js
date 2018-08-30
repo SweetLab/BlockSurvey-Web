fdf8bb965036a532a106a1c8f9faceb728f2c2a0
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-30 22:05:56 +0900

    Add Link

--- src/components/Mypage/TopBar.js
+++ src/components/Mypage/TopBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './css/TopBar.css';
 
 class TopBar extends Component {
     render() {
         return (
             <div className="MypageTopbar">
-                <div className="Logo"></div>
+                <Link to="/">
+                    <div className="Logo"></div>
+                </Link>
                 <div className="filter"></div>
                 <div className="Menu"></div>
             </div>

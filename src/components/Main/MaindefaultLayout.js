c0fdef5f1cd8845d664d8d095ee408bc3dd6909b
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-30 19:59:16 +0900

    스타일 수정

--- src/components/Main/MaindefaultLayout.js
+++ src/components/Main/MaindefaultLayout.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class MaindefaultLayout extends Component {
   render() {
-    const {pageId} = this.props;
+    const { pageId } = this.props;
     return (
-      <div className="section" id={`page${pageId}`}>
-        {this.props.children}
+      <div className="section" id = { `page${ pageId }` }>
+        { this.props.children }
       </div>
     );
   }

8e41134ea0b0bcb58b2d834ce8c70670c160d1f5
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-30 19:59:55 +0900

    Delete code

--- src/components/Participate/ParticipateJoin.js
+++ src/components/Participate/ParticipateJoin.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
-import { Logo, Blackmenubtn, DownArrowBtn } from '../../assets/assets'
 import './css/ParticipateJoin.css';
 
 class Participate extends Component {
     render() {
         return (
             <div className="Participate_Join">
-                <header className="Topbar">
-                    <div className="Logo">
-                        <img src={Logo} alt="로고"/>
-                    </div>
-                    <div className="TopBtns">
-                        <div className="Down_Arrow">
-                            <img src={DownArrowBtn} alt="아래_화살표"/>
-                        </div>
-                        <div className="MenuBtn">
-                            <img src={Blackmenubtn} alt="메뉴버튼"/>
-                        </div>
-                    </div>
-                </header>
                 {this.props.children}
             </div>
         );

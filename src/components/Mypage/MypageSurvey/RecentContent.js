959f9ae694653ca679570c9ba2f6bf0581bb1b6f
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-31 17:46:15 +0900

    컴포넌트 추가

---
+++ src/components/Mypage/MypageSurvey/RecentContent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Survey_BlackUser, Survey_Participate_people } from '../../../assets/assets';
+import './css/RecentContent.css'
+
+class RecentContent extends Component {
+    render() {
+        return (
+            <div className="_Survey_ParticipateUser">
+                <div className="_SurveyBox">
+                    <div className="_SurveyInfor">
+                        <div className="_SurveyTitle">
+                            What is love
+                        </div>
+                        <span className="_SurveyUser" >qudtls123</span>
+                        <div className="_SurveyMember">
+                            <img className="_BlackUser" src={Survey_BlackUser} alt="검은색 사람 이미지"/>
+                            <span className="_UserCount">1,000,000</span>
+                        </div>
+                    </div>
+                    <div className="filter"></div>
+                    <div className="_SurveyICON">
+                        <div className="_SurveyJoiner">
+                            <img className="_BlueUser" src={Survey_Participate_people} alt="파란색 사람 이미지"/>
+                            <div className="_MemberCount">15/15</div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        );
+    }
+}
+
+export default RecentContent;

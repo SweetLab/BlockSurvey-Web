d2706b9048be7ce6c2a71e7e96dc281ad8d19d40
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-31 11:12:09 +0900

    Add DrobDown

---
+++ src/core/DropDown/Dropdown.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Dropdown from 'react-dropdown'
+import './dropdown.css'
+
+const options = [
+  '객관식', '단일입력란'
+]
+
+const arrowClosed = (
+  <span className="arrow-closed" />
+)
+const arrowOpen = (
+  <span className="arrow-open" />
+)
+
+
+class Dropdowns extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: ''
+    }
+    this._onSelect = this._onSelect.bind(this)
+  }
+
+  _onSelect (option) {
+    this.setState({selected: option})
+  }
+
+  render() {
+    const defaultOption = this.state.selected
+
+    return (
+        <Dropdown 
+          className="DropDown"  
+          options={options} 
+          onChange={this._onSelect} 
+          value={defaultOption} 
+          placeholder="선택" 
+          arrowClosed={arrowClosed}
+          arrowOpen={arrowOpen}
+        />
+    )
+  }
+}
+
+export default Dropdowns;

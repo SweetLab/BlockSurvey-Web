d699b207f0e678a790dd4eeaf70ad0a4cc1e7a67
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-31 17:45:23 +0900

    Add component

--- src/components/Mypage/index.js
+++ src/components/Mypage/index.js
@@ -2,5 +2,6 @@ import MypageNavi from './MypageNavi';
 import MypageDefaultLayout from './MypageDefaultLayout'
 import MypageTopBar from './MypageTopBar'
 import MypageMain from './MypageMain'
+import MypageSurvey from './MypageSurvey/MypageSurvey'
 
-export { MypageNavi, MypageMain, MypageDefaultLayout, MypageTopBar };
+export { MypageNavi, MypageMain, MypageDefaultLayout, MypageTopBar, MypageSurvey };
